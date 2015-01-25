f5fecdf2c60849d4df56d84301922e4f87ba587a
Linlin Yan <yanlinlin82@gmail.com>
2015-01-25 14:14:04 +0800

    Show alignment detail.

--- src/command_map.c
+++ src/command_map.c
@@ -4,11 +4,12 @@
 #include "ref_map.h"
 
 #define DEF_TOLERANCE 0.1
-#define DEF_MIN_MATCH 4
+#define DEF_MIN_MATCH 3
 
 static int verbose = 0;
 static double tolerance = DEF_TOLERANCE;
 static int min_match = DEF_MIN_MATCH;
+static int output_align = 0;
 
 static void print_usage(void)
 {
@@ -21,28 +22,106 @@ static void print_usage(void)
 			"   -v           show verbose message\n"
 			"   -e <FLOAT>   tolerance to compare fragment size [%f]\n"
 			"   -m <INT>     minimal matched fragment [%d]\n"
+			"   -a           output alignment detail\n"
 			"\n", DEF_TOLERANCE, DEF_MIN_MATCH);
 }
 
 static void print_header(void)
 {
-	printf("#name\tchrom\tpos\tstrand\tsize\tlabels\trstart\trend\tqstart\tqend\n");
+	printf("#name\tchrom\tpos\tstrand\tsize\tlabels\t"
+			"qsize\tqlabels\trstart\trend\tqstart\tqend\n");
 }
 
-static void print_item(const char *name, const char *chrom, int pos, int direct,
-		int size, size_t labels, size_t rstart, size_t rend, size_t qstart, size_t qend)
+static inline size_t max(size_t a, size_t b) { return (a >= b ? a : b); }
+
+static int get_number_width(int x)
 {
-	printf("%s\t%s\t%d\t%s\t%d\t%zd\t%zd\t%zd\t%zd\t%zd\n",
-			name, chrom, pos, (direct > 0 ? "+" : "-"),
-			size, labels, rstart, rend, qstart, qend);
+	int width = 1;
+	while (x >= 10) {
+		x /= 10;
+		++width;
+	}
+	return width;
 }
 
-static void map(struct ref_map *ref, struct fragment *qry_item)
+static void output_item(const struct ref_map *ref, const struct fragment *qry,
+		size_t rindex, size_t qindex, int direct, size_t count)
+{
+	const char *qname = qry->name;
+	const char *rname = ref->map.fragments.data[ref->nodes[rindex].chrom].name;
+	size_t rstart = (direct > 0 ? rindex : rindex - count + 1);
+	size_t rend = (direct > 0 ? rindex + count : rindex + 1);
+	size_t qstart = (direct > 0 ? qindex : qindex + count - 1);
+	size_t qend = (direct > 0 ? qindex + count : qindex - 1);
+
+	printf("%s\t", qname);
+	printf("%s\t", rname);
+	printf("%d\t", ref->nodes[rstart].pos);
+	printf("%s\t", (direct > 0 ? "+" : "-"));
+	printf("%d\t", ref->nodes[rend].pos - ref->nodes[rstart].pos);
+	printf("%zd\t", count);
+	printf("%d\t", qry->nicks.data[qindex + count].pos - qry->nicks.data[qindex].pos);
+	printf("%zd\t", count);
+	printf("%zd\t", rstart);
+	printf("%zd\t", rend);
+	printf("%zd\t", qstart);
+	printf("%zd\n", qend);
+
+	if (output_align) {
+		int name_width, pos_width;
+		size_t i;
+		int *q = malloc(sizeof(int) * count);
+		int *r = malloc(sizeof(int) * count);
+		int *w = malloc(sizeof(int) * count);
+
+		name_width = (int)max(strlen(rname), strlen(qname));
+		pos_width = get_number_width(max(max(rstart, rend), max(qstart, qend)));
+
+		for (i = 0; i < count; ++i) {
+			r[i] = ref->nodes[rstart + i].pos - ref->nodes[rstart + i - 1].pos;
+			q[i] = qry->nicks.data[qstart + direct * i].pos
+					- qry->nicks.data[qstart + direct * i - 1].pos;
+			w[i] = get_number_width(max(r[i], q[i]));
+		}
+
+		printf(" %*s: ", name_width, rname);
+		for (i = 0; i < count; ++i) {
+			printf("%*zd", pos_width, rstart + i);
+			if (i + 1 < count) {
+				printf(" -(%*d)- ", w[i], r[i]);
+			}
+		}
+		printf("\n");
+
+		printf(" %*s  ", name_width, "");
+		for (i = 0; i < count; ++i) {
+			printf("%*c", pos_width, '|');
+			if (i + 1 < count) {
+				printf("  (%*d)  ", w[i], q[i] - r[i]);
+			}
+		}
+		printf("\n");
+
+		printf(" %*s: ", name_width, qname);
+		for (i = 0; i < count; ++i) {
+			printf("%*zd", pos_width, qstart + direct * i);
+			if (i + 1 < count) {
+				printf(" -(%*d)- ", w[i], q[i]);
+			}
+		}
+		printf("\n");
+
+		free(w);
+		free(r);
+		free(q);
+	}
+}
+
+static void map(const struct ref_map *ref, struct fragment *qry_item)
 {
 	size_t i, j, index;
-	int *intervals;
 	int direct;
-	size_t qstart = 1;
+	size_t qstart;
 
 	if (qry_item->nicks.size < min_match + 2) {
 		if (verbose) {
@@ -51,33 +130,26 @@ static void map(struct ref_map *ref, struct fragment *qry_item)
 		return;
 	}
 
-	intervals = malloc(sizeof(int) * qry_item->nicks.size);
-	for (i = 0; i < qry_item->nicks.size; ++i) {
-		intervals[i] = qry_item->nicks.data[i].pos - (i == 0 ? 0 : qry_item->nicks.data[i - 1].pos);
-	}
+	qstart = 2 /* skip first fragment */;
+	while (qstart + 1 < qry_item->nicks.size) { /* skip last fragment */
+		const struct nick *p = &qry_item->nicks.data[qstart];
+		int fragment_size = p->pos - (p - 1)->pos;
 
-	while (qstart + 1 < qry_item->nicks.size) {
 		size_t max_count = 0;
 		for (i = 0; i < ref->size; ++i) {
-			if (ref->index[i]->size < intervals[qstart] * (1 - tolerance)) continue;
-			if (ref->index[i]->size > intervals[qstart] * (1 + tolerance)) break;
+			if (ref->index[i]->size < fragment_size * (1 - tolerance)) continue;
+			if (ref->index[i]->size > fragment_size * (1 + tolerance)) break;
 
 			index = ref->index[i] - ref->nodes;
 
 			for (direct = 1; direct >= -1; direct -= 2) {
 				for (j = 1; j < qry_item->nicks.size; ++j) {
-					if (ref->nodes[index + direct * j].size < intervals[qstart + j] * (1 - tolerance)) break;
-					if (ref->nodes[index + direct * j].size > intervals[qstart + j] * (1 + tolerance)) break;
+					int fragment_size = (p + j)->pos - (p + j - 1)->pos;
+					if (ref->nodes[index + direct * j].size < fragment_size * (1 - tolerance)) break;
+					if (ref->nodes[index + direct * j].size > fragment_size * (1 + tolerance)) break;
 				}
-				if (j >= min_match) {
-					int size = ref->nodes[index + direct * j].pos - ref->nodes[index].pos;
-					if (size < 0) size = -size;
-
-					print_item(qry_item->name,
-							ref->map.fragments.data[ref->index[i]->chrom].name,
-							ref->index[i]->pos, direct,
-							size, j, index, index + direct * (j - 1),
-							qstart, qstart + j - 1);
+				if (j > min_match) {
+					output_item(ref, qry_item, index, qstart, direct, j);
 					if (max_count < j) {
 						max_count = j;
 					}
@@ -91,7 +163,7 @@ static void map(struct ref_map *ref, struct fragment *qry_item)
 static int check_options(int argc, char * const argv[])
 {
 	int c;
-	while ((c = getopt(argc, argv, "ve:m:")) != -1) {
+	while ((c = getopt(argc, argv, "ve:m:a")) != -1) {
 		switch (c) {
 		case 'v':
 			++verbose;
@@ -101,6 +173,9 @@ static int check_options(int argc, char * const argv[])
 			break;
 		case 'm':
 			min_match = atoi(optarg);
+			break;
+		case 'a':
+			output_align = 1;
 			break;
 		default:
 			return 1;
