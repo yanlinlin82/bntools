6980b1a258adc30d2cb721e3686c568a29c4eeca
Linlin Yan <yanlinlin82@gmail.com>
2015-01-26 09:55:55 +0800

    Remove unused function.

--- src/command_align.c
+++ src/command_align.c
@@ -32,7 +32,6 @@ struct node {
 };
 
 static inline int min(int a, int b) { return (a < b ? a : b); }
-static inline int max(int a, int b) { return (a > b ? a : b); }
 
 static int align(const struct fragment *fa, const struct fragment *fb)
 {
